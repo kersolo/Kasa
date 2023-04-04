a8e283538b2f9bd92f208860d68665c651669bef
kersolo <loick.ferelloc@gmail.com>
2023-04-04 15:29:35 +0200

    projet avant soutenance

--- src/App.js
+++ src/App.js
@@ -10,7 +10,6 @@ function App() {
         <Header />
         <Router />
       </div>
-
       <Footer />
     </>
   );
--- src/pages/404/ErrorPage.jsx
+++ src/pages/404/ErrorPage.jsx
@@ -16,25 +16,3 @@ function ErrorPage() {
 }
 
 export default ErrorPage;
-
-//////////////////////////////////////////
-// import { Link } from 'react-router-dom';
-// import './errorPage.css';
-
-// function ErrorPage() {
-//   return (
-//     <main>
-//       <article className="error-container">
-//         <h1 className="title-error">404</h1>
-//         <p className="text-error">
-//           Oups! La page que vous demandez n'existe pas.
-//         </p>
-//         <Link to="/" className="Link-error">
-//           Retourner sur la page d'accueil
-//         </Link>
-//       </article>
-//     </main>
-//   );
-// }
-
-// export default ErrorPage;
